46b6676b6cb771e8ffd08859658a90655768647e
Jonas Mendes <jonas.ribeiro@gmail.com>
2018-10-05 13:22:44 -0300

    Initial Commit

---
+++ Jokenpo/Jokenpo/Classes/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokenpo.Classes
+{
+    public class Game
+    {
+        public enum Option
+        {
+            Rock,
+            Paper,
+            Sissors,
+        }
+
+        /// <summary>
+        /// Checks whether the player has won from your opponent
+        /// </summary>
+        /// <param name="player">Player</param>
+        /// <param name="oponnent">Oponnet</param>
+        /// <returns></returns>
+        private bool PlayerWinner(Option player, Option oponnent)
+        {
+            if (player == Option.Paper && oponnent == Option.Rock) return true;
+            if (player == Option.Rock && oponnent == Option.Sissors) return true;
+            if (player == Option.Sissors && oponnent == Option.Paper) return true;
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player1"></param>
+        /// <param name="player2"></param>
+        /// <returns></returns>
+        public Option? CheckWinner(Option player1, Option player2)
+        {
+            Option? result = null;
+            try
+            {
+                if (player1 != player2)
+                {
+                    if (PlayerWinner(player1, player2)) result = player1;
+                    if (PlayerWinner(player2, player1)) result = player2;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Jokenpo/UnitTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jokenpo.Classes;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGameDraw()
+        {
+            Game game = new Game();
+
+            try
+            {
+                Assert.IsNull(game.CheckWinner(Jokenpo.Classes.Game.Option.Paper, Jokenpo.Classes.Game.Option.Paper));
+                Assert.IsNull(game.CheckWinner(Jokenpo.Classes.Game.Option.Rock, Jokenpo.Classes.Game.Option.Rock));
+                Assert.IsNull(game.CheckWinner(Jokenpo.Classes.Game.Option.Sissors, Jokenpo.Classes.Game.Option.Sissors));
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestGamePlayer1Won()
+        {
+            Game game = new Game();
+
+            try
+            {
+                Assert.AreEqual(game.CheckWinner(Jokenpo.Classes.Game.Option.Paper, Jokenpo.Classes.Game.Option.Rock), Jokenpo.Classes.Game.Option.Paper);
+                Assert.AreEqual(game.CheckWinner(Jokenpo.Classes.Game.Option.Rock, Jokenpo.Classes.Game.Option.Sissors), Jokenpo.Classes.Game.Option.Rock);
+                Assert.AreEqual(game.CheckWinner(Jokenpo.Classes.Game.Option.Sissors, Jokenpo.Classes.Game.Option.Paper), Jokenpo.Classes.Game.Option.Sissors);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestGamePlayer2Won()
+        {
+            Game game = new Game();
+
+            try
+            {
+                Assert.AreEqual(game.CheckWinner(Jokenpo.Classes.Game.Option.Rock, Jokenpo.Classes.Game.Option.Paper), Jokenpo.Classes.Game.Option.Paper);
+                Assert.AreEqual(game.CheckWinner(Jokenpo.Classes.Game.Option.Sissors, Jokenpo.Classes.Game.Option.Rock), Jokenpo.Classes.Game.Option.Rock);
+                Assert.AreEqual(game.CheckWinner(Jokenpo.Classes.Game.Option.Paper, Jokenpo.Classes.Game.Option.Sissors), Jokenpo.Classes.Game.Option.Sissors);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
